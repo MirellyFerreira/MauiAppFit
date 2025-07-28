ab58f284d92fb09ed57de672ccfa0912f99cb1ea
MIRELLY FERREIRA <mirelly.ferreira@etec.sp.gov.br>
2025-07-28 11:38:06 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppFit/Helpers/SQLiteDataBaseHelper.cs
@@ -0,0 +1,47 @@
+﻿using __XamlGeneratedCode__;
+using MauiAppFit.Models;
+using SQLite;
+
+namespace MauiAppFit.Helpers
+{
+    public class SQLiteDataBaseHelper
+    {
+        readonly SQLiteAsyncConnection_db;
+
+        public SQLiteDataBaseHelper(string dbPath)
+        {
+            _db = new SQLiteAsyncConnection(dbPath);
+            _db.CreateTableAsync<Atividade>().Wait();
+        }
+
+        public Task<List<Atividade>> GetAllRows()
+        {
+            return _db.Table<Atividade>().OrderByDescending(i=> i.Id).ToListAsync();
+        }
+
+        public Task<Atividade> GetById(int id)
+        {
+            return _db.Table<Atividade>().FirstAsync(in => id.Id == id);
+        }
+
+        public Task<int> Insert(Atividade model)
+        {
+            return _db.InsertAsync(model);
+        }
+        
+        public Task<List<Atividade>> Update(Atividade model)
+        {
+            string sql = "UPDATE Atividade SET Descricao=?, Data=?, Peso=?,  " + "Observacoes=? WHERE Id=?";
+
+            return _db.QueryAsync<Atividade>(
+                sql,
+                model.Descricao,
+                model.Data,
+                model.Peso,
+                model.Observacoes,
+                model.Id
+            );
+        }
+
+    }
+}
